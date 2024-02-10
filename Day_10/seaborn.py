1553ffce9dfed9b15cf3c4b6adddbd62d07a2803
Dharmendeer <saferzone56@gmail.com>
2024-02-10 14:17:35 -0500

    Using pandas lib & Searborn and Metplotlib Usinf Graphical represtention

---
+++ Day_10/seaborn.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import pandas as pd 
+import numpy as np
+import matplotlib as plt
+roll_no =[1,2,3,4,5,6,7,8,8,9,9,7]
+marks = [9,8,65,4,3,2,1,1,6,7,8,9]
+sample_df =pd.DataFrame({"Rollno":roll_no, "Marks":marks})
+print(sample_df.head())
