73374338657c6c4d69d5e00dd26ad14973a305a8
Dharmender <138750969+Dharmendeer@users.noreply.github.com>
2024-06-17 19:29:35 +0530

    Update seaborn.py

--- Day_10/seaborn.py
+++ Day_10/seaborn.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 roll_no =[1,2,3,4,5,6,7,8,8,9,9,7]
-marks = [9,8,65,4,3,2,1,1,6,7,8,9]
+marks = [9,8,65,4,3,2,1,,1,6,7,8,9]
 sample_df =pd.DataFrame({"Rollno":roll_no, "Marks":marks})
 print(sample_df.head())
